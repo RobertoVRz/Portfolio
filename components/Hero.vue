fb9f8a8d5ecfd31baeaeb343b64d8e2331c86249
RobertoVrz <roberto.vzrz@gmail.com>
2021-06-05 03:08:17 -0500

    First mobile configs

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,11 +1,18 @@
 <template>
   <div class="bg-black h-screen relative flex items-center justify-center">
-    <div class="flex flex-row">
-      <h1 class="text-white text-7xl">Hola, soy Roberto</h1>
-      <img class="w-96" src="@/assets/AvatarBig.png" />
+    <div class="flex flex-col items-center md_items-start md_flex-row">
+      <h1 class="text-white text-base md_text-7xl">Hola, soy Roberto</h1>
+      <img class="w-56 md_w-96 z-10 mx-10" src="@/assets/AvatarBig.png" />
     </div>
 
-    <img class="w-screen absolute bottom-0 -mb-1" src="@/assets/wave.svg" />
+    <img
+      class="hidden md_block w-screen absolute bottom-0 -mb-1"
+      src="@/assets/wave.svg"
+    />
+    <img
+      class="md_hidden block w-screen absolute bottom-0"
+      src="@/assets/waveMobile.svg"
+    />
   </div>
 </template>
 
